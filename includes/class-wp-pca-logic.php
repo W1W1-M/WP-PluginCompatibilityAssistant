f3a3c975cc7f7b1dac860cd751f36412c57b7bae
W1W1-M <w1w1_m@icloud.com>
2023-04-11 20:57:39 +0200

    Added plugins_url, wordpress_version, php_version variables & methods.

--- includes/class-wp-pca-logic.php
+++ includes/class-wp-pca-logic.php
@@ -1,21 +1,37 @@
 <?php
 
 require_once ABSPATH . 'wp-admin/includes/plugin.php';
+require_once ABSPATH . 'wp-includes/general-template.php';
 
 class WP_PCA_Logic {
 
-    private array $installed_plugins = [];
-    private ?int $installed_plugins_count = NULL;
+    // variables
+
+    private array $installed_plugins;
+    private ?int $installed_plugins_count;
+    private ?string $plugins_url;
+    private ?string $wordpress_version;
+    private ?string $php_version;
+
+    // class methods
 
     public function __construct() {
         $this->installed_plugins = [];
         $this->installed_plugins_count = NULL;
+        $this->plugins_url = NULL;
+        $this->wordpress_version = NULL;
+        $this->php_version = NULL;
     }
 
     public function run() {
         $this->get_installed_plugins();
         $this->count_installed_plugins();
+        $this->get_plugins_url();
+        $this->get_wordpress_version();
+        $this->get_php_version();
     }
+
+    // installed_plugins methods
 
     public function get_installed_plugins() {
         $this->installed_plugins = get_plugins();
@@ -24,6 +40,38 @@ class WP_PCA_Logic {
 
     public function count_installed_plugins() {
         $this->installed_plugins_count = count($this->installed_plugins);
+    }
+
+    // plugins_url methods
+
+    public function get_plugins_url() {
+        $this->plugins_url = plugins_url();
+        return $this->plugins_url;
+    }
+
+    public function print_plugins_url() {
+        echo $this->plugins_url;
+    }
+
+    // wordpress_version methods
+
+    public function get_wordpress_version() {
+        $this->wordpress_version = get_bloginfo('version');
+    }
+
+    public function print_wordpress_version() {
+        echo $this->wordpress_version;
+    }
+
+    // php_version methods
+
+    public function get_php_version() {
+        $this->php_version = phpversion();
+        return $this->php_version;
+    }
+
+    public function print_php_version() {
+        echo $this->php_version;
     }
 }
 
